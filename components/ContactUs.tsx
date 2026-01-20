e03cdaf0eeb315b0966decd5918c9b59e61cb776
akshithnallaginnela <akshithdeekshith@gmail.com>
2026-01-20 13:23:23 +0530

    feat: add ContactUs component with form submission, contact information, and social links.

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -25,7 +25,7 @@ const ContactUs: React.FC<ContactUsProps> = ({ onBack }) => {
 
         try {
             // Submit to Formspree
-            const response = await fetch('https://formspree.io/f/YOUR_FORMSPREE_ID', {
+            const response = await fetch('https://formspree.io/f/xaqqbdpn', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
